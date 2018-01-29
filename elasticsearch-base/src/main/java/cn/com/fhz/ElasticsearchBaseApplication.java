30b688412455f2c0cf3f3eff6eb23a2faee976d4
woniyu <1010569623@qq.com>
2018-01-29 21:56:22 +0800

    增加基础操作

---
+++ elasticsearch-base/src/main/java/cn/com/fhz/ElasticsearchBaseApplication.java
@@ -0,0 +1,12 @@
+package cn.com.fhz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchBaseApplication.class, args);
+	}
+}
--- elasticsearch-base/src/main/java/cn/com/fhz/service/impl/AdServiceImpl.java
+++ elasticsearch-base/src/main/java/cn/com/fhz/service/impl/AdServiceImpl.java
@@ -1,25 +1,25 @@
 package cn.com.fhz.service.impl;
 
-import cn.com.fhz.dao.AdDAO;
+import cn.com.fhz.dao.AdDao;
 import cn.com.fhz.entity.AdEntity;
 import cn.com.fhz.service.AdService;
-import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
- * Created by hzfang on 2018/1/29.
+ * Created by woni on 18/1/29.
  */
 @Service
 public class AdServiceImpl implements AdService {
 
     @Autowired
-    AdDAO adDAO;
+    AdDao adDao;
+
 
     @Override
-    public List<AdEntity> finaAll() {
-        return adDAO.findAll();
+    public List<AdEntity> findAll() {
+        return adDao.findAll();
     }
 }
