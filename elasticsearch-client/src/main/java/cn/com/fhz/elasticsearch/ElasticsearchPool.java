5c06164fbc8a98b80e9c687e979fca6036115462
woniyu <1010569623@qq.com>
2018-02-07 21:53:16 +0800

    去除掉验证，防止微服务调用的时候，出现错误

--- elasticsearch-client/src/main/java/cn/com/fhz/elasticsearch/ElasticsearchPool.java
+++ elasticsearch-client/src/main/java/cn/com/fhz/elasticsearch/ElasticsearchPool.java
@@ -1,17 +1,17 @@
-package cn.com.fhz.elasticsearch;
-
-import cn.com.fhz.config.Config;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-/**
- * Created by hzfang on 2018/1/26.
- * 后期准备做连接池设置
- */
-@Component
-public class ElasticsearchPool {
-
-    @Autowired
-    Config config;
-
-}
+//package cn.com.fhz.elasticsearch;
+//
+//import cn.com.fhz.config.Config;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * Created by hzfang on 2018/1/26.
+// * 后期准备做连接池设置
+// */
+//@Component
+//public class ElasticsearchPool {
+//
+//    @Autowired
+//    Config config;
+//
+//}
